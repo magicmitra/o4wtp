1223cd15b9fa31083f012ffdb10ea4428910c053
erverted <ervinmitra@gmail.com>
2023-05-11 22:00:25 -0500

    prep hero

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
 
     return (
         <div className='Hero-image'>
-            <img src='/images/wine/hero.avif'/>
+            <img src='/images/wine/hero.jpg'/>
         </div>
     )
 }
