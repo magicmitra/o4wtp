b8fd8507de0d1b1a3040f3758d0f7aa5510b0bb0
erverted <ervinmitra@gmail.com>
2023-05-15 15:44:32 -0500

    add all these bullshit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
 import LandingPage from './pages/Landing';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <LandingPage/>
     </div>
   );
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,10 @@
 import { Button, ThemeProvider } from '@mui/material'
+import styles from  '../App.module.css'
 
 const LandingPage = () => {
 
     return (
-        <div className='Hero-image'>
+        <div className={styles['Hero-image']}>
             <img src='/images/wine/hero.jpg'/>
         </div>
     )
